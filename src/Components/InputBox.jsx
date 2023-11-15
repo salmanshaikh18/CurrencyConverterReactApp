6dc727f8a9425110f6691195505bfd976b3f5b72
Salman <salmanshaikh18786@gmail.com>
2023-11-15 12:46:57 +0530

    HomePage Structure

---
+++ src/Components/InputBox.jsx
@@ -0,0 +1,11 @@
+
+
+const InputBox = () => {
+    return (
+        <div className="bg-white rounded-lg h-[40%] m-3">
+            InputBox
+        </div>
+    )
+}
+
+export default InputBox
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import InputBox from '../Components/InputBox'
 
 const HomePage = () => {
   return (
     <div className='h-[100vh] w-[100vw] bg-slate-600 text-[#fff] flex justify-center items-center'>
-        <div className='bg-gray-400 h-[60%] w-[90%] rounded-lg 2xl:w-[40%] xl:w-[50%] lg:w-[60%] md:w-[70%] sm:w-[80%]'>
+        <div className='bg-gray-400 h-[65%] w-[90%] rounded-lg 2xl:w-[40%] xl:w-[50%] lg:w-[60%] md:w-[70%] sm:w-[80%]'>
+            <InputBox />
+            <InputBox />
+            <div className='flex justify-center items-center m-3'>
+                <button className='text-center bg-green-500 rounded p-2 w-[100%] '>Convert USD to INR</button>
+            </div>
             
         </div>
     </div>
