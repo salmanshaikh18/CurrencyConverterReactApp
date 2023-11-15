d4fe5478bb1418b4348cd45ed5b48008d38b3024
Salman <salmanshaikh18786@gmail.com>
2023-11-15 15:15:10 +0530

    update

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -1,11 +1,13 @@
 
 
-const InputBox = () => {
+const InputBox = ({
+    label
+}) => {
     return (
         <div className="bg-gray-300 rounded-lg h-[35%] w-[100%] mr-5 ml-5 m-4 flex p-2 justify-between items-center">
             <div className="flex flex-col text-black w-[50%]">
-                <label htmlFor="">From</label>
-                <input className="w-[100%] rounded p-2 border-none outline-none" type="text" placeholder="Amount.."/>
+                <label htmlFor="">{label}</label>
+                <input className="w-[100%] rounded p-2 border-none outline-none" type="text" placeholder="Amount.." />
             </div>
             <div className="text-black w-[50%] text-right">
                 <p>Currency Type</p>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
 import InputBox from '../Components/InputBox'
 
 const HomePage = () => {
   return (
-    <div className='h-[100vh] w-[100vw] bg-slate-600 text-[#fff] flex justify-center items-center'>
+    <div className='h-[100vh] w-[100vw] bg-slate-600 text-[#fff] flex flex-col items-center'>
+        <h1 className='text-green-500 text-3xl mb-[3.5rem] mt-10 font-bold '>Currency Coverter</h1>
         <div className='bg-gray-400 h-[55vh] w-[90vw] rounded-lg 2xl:w-[40%] xl:w-[50%] lg:w-[60%] md:w-[70%] sm:w-[80%] flex flex-col justify-center items-center pr-4 pl-5'>
-            <InputBox />
-            <button className='text-center bg-green-500 p-2 pl-5 pr-5 rounded z-10 m-[-32px]'>Swap</button>
-            <InputBox />
-            <div className='flex justify-center items-center m-3'>
-                <button className='text-center bg-green-500 rounded p-2 w-[100%] '>Convert USD to INR</button>
+            <InputBox label="From" />
+            <button className='text-center bg-green-500 p-2 pl-5 pr-5 rounded-lg z-10 m-[-32px] hover:bg-green-800 transition ease-in-out duration-500'>Swap</button>
+            <InputBox label="To" />
+            <div className='flex justify-center items-center m-3 w-[100%]'>
+                <button className='text-center bg-green-500 rounded-lg p-2 w-[100%] transition ease-in-out duration-500 hover:bg-green-800'>Convert USD to INR</button>
             </div>
             
         </div>
