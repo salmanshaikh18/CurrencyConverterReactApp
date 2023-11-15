e14f88002ff9b0c7921524a0a44f124218e980b2
Salman <salmanshaikh18786@gmail.com>
2023-11-15 13:55:21 +0530

    update responsive

--- src/Components/InputBox.jsx
+++ src/Components/InputBox.jsx
@@ -2,7 +2,7 @@
 
 const InputBox = () => {
     return (
-        <div className="bg-gray-300 rounded-lg h-[35%] w-[90%] sm:w-[95%] m-4">
+        <div className="bg-gray-300 rounded-lg h-[35%] w-[100%] mr-5 ml-5 m-4">
             InputBox
         </div>
     )
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,7 +4,7 @@ import InputBox from '../Components/InputBox'
 const HomePage = () => {
   return (
     <div className='h-[100vh] w-[100vw] bg-slate-600 text-[#fff] flex justify-center items-center'>
-        <div className='bg-gray-400 h-[55vh] w-[90vw] rounded-lg 2xl:w-[40%] xl:w-[50%] lg:w-[60%] md:w-[70%] sm:w-[80%] flex flex-col justify-center items-center'>
+        <div className='bg-gray-400 h-[55vh] w-[90vw] rounded-lg 2xl:w-[40%] xl:w-[50%] lg:w-[60%] md:w-[70%] sm:w-[80%] flex flex-col justify-center items-center pr-4 pl-5'>
             <InputBox />
             <button className='text-center bg-green-500 p-2 pl-5 pr-5 rounded z-10 m-[-32px]'>Swap</button>
             <InputBox />
